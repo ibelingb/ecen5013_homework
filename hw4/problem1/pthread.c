d86c573341c70a12e28ae7844126729bb9a24631
Brian <ibelingb@colorado.edu>
2019-02-23 19:15:42 -0700

    Adding updated makefile for hw4.

---
+++ hw4/problem1/pthread.c
@@ -0,0 +1,27 @@
+/* ECEN5013 - HW4
+ * Date: 2/28/2019
+ * Author: Brian Ibeling
+ * About: C program to demonstrate multithreading and sharing data between
+ *        multiple threads via a file.
+ */
+/*
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+
+int main() {
+  FILE* pFile = NULL;
+  char* filename = "shared_file.txt";
+  int status = 0;
+  size_t len = 0;
+
+
+
+  return 0;
+}
